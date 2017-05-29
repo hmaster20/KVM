f52925c49ceaba01cd3023e74871620a1e72d6df
Sergei <hmaster20@gmail.com>
2017-05-29 17:17:55 +0300

    cron mod

--- cron_backup_vm.bash
+++ cron_backup_vm.bash
@@ -1,3 +1,4 @@
 # Ежедневный бэкап VM
-# Предварительно выполнить команду: chmod +x kvm_backup_vm.sh
+# Файл задания должен быть размещен в /etc/cron.d
+# Предварительно выполнить команду для скрипта: chmod +x kvm_backup_vm.sh
 00 01 * * * root /root/kvm_backup_vm.sh DV_5.4_KRD_Test >> /mnt/md0/logs/log_backup_vm.log
